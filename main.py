39d16ba8eae94c774c54a15a128c1cf334db82d6
Markelov A <mrklv001@gmail.com>
2025-04-06 20:19:16 +0300

    Added processing extract URL path

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import socket
 
+# Parse the request data to extract the HTTP method, path and version
+def parse_request(request_data):
+    lines = request_data.split('\r\n')
+    start_line = lines[0]
+    method, path, version = start_line.split(' ')
+    return method, path, version
 
+
+# Returns the HTTP response for a given path
+def get_response(path):
+    responses = {
+        "/": "HTTP/1.1 200 OK\r\n\r\n",
+    }
+    
+    # Default response if path not found
+    default_response = "HTTP/1.1 404 Not Found\r\n\r\n"
+
+    return responses.get(path, default_response)
+
+
+# Returns the HTTP response for a given path
 def handle_request(client_socket):
-    client_socket.recv(1024) # Reading a bit of data 
-
-    # Send a 200 OK response
-    response = "HTTP/1.1 200 OK\r\n\r\n"
+    request_data = client_socket.recv(1024).decode()
+    if not request_data:
+        return
+    
+    method, path, version = parse_request(request_data)
+    response = get_response(path)
     client_socket.send(response.encode())
 
 def main():
@@ -25,7 +47,7 @@ def main():
 
             client_socket.close()
     except KeyboardInterrupt:
-        print("\Server is shutting down.")
+        print("\nServer is shutting down.")
     finally:
         server_socket.close()
         print("Server has been shut down.")
